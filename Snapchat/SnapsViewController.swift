a4634f92f3354ec206313601e99d838bf2358861
Mohammed Al-Khonaizi <backlife2005@gmail.com>
2017-03-07 20:07:25 +0300

    Added a swift file for SnapsViewController.
    Added a button named Logout/
    
    Added an @IBAction for the logoutTapped action.
    
    Added the dismiss method to return back to the Signing In View Controller.

---
+++ Snapchat/SnapsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SnapsViewController.swift
+//  Snapchat
+//
+//  Created by Mohammed Al-Khonaizi on 3/7/17.
+//  Copyright © 2017 Mohammed Al-Khonaizi. All rights reserved.
+//
+
+import UIKit
+
+class SnapsViewController: UIViewController {
+    
+    
+    @IBAction func logoutTapped(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
