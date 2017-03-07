5418d8f80925fabb1e0b45068b5891d8a69aaad4
Mohammed Al-Khonaizi <backlife2005@gmail.com>
2017-03-07 16:31:51 +0300

    The application is functional and run ok after the following changes:
    AppDelegate.swift
    
    - Imported the Firebase
    
    - Added the “FIRApp.configure()” line of code.

--- Snapchat/AppDelegate.swift
+++ Snapchat/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+import Firebase
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,6 +18,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        // Firebase
+        
+        FIRApp.configure()
+        
         return true
     }
 
