4cb1a625449bbc90b8bb786f35cf85f061b3301e
Mohammed Al-Khonaizi <backlife2005@gmail.com>
2017-03-08 16:23:00 +0300

    Added an Add button the SnapsViewController.
    Added a new Picture View Controller.
    
    Added a swift file the new PictureViewController.
    
    Added a segue to switch from the SnapsViewController to the PictureViewControlle.
    
    Added a Camera button.
    
    Added an ImageView.
    
    Added a Text Field for the Description.
    
    Added a button and set its tile to Next.
    
    Added three @IBOutlet for (imageView, descriptionTextField and nextButton)
    
    Added two @IBAction for (cameraTapped and nextTapped).

---
+++ Snapchat/PictureViewController.swift
@@ -0,0 +1,41 @@
+//
+//  PictureViewController.swift
+//  Snapchat
+//
+//  Created by Mohammed Al-Khonaizi on 3/8/17.
+//  Copyright © 2017 Mohammed Al-Khonaizi. All rights reserved.
+//
+
+import UIKit
+
+class PictureViewController: UIViewController {
+
+    
+    // @IBOutlet
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    
+    // @IBAction
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func nextTapped(_ sender: Any) {
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
