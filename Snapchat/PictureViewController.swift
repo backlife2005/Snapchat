5d276fea094dd795efa9ad3008b977dc492b7254
Mohammed Al-Khonaizi <backlife2005@gmail.com>
2017-03-09 04:46:07 +0300

    Added a new View Controller to show the Snapchat users.
    Added a segue named “selectUserSegue” from to PictureViewController to the View Controller which shows the Users.
    
    Imported the Firebase.
    
    Updated the nextTapped action to do the following.
    // 1) Disables the Next button until the selected image is upload to the Storage of Firebase
    // 2) Sets the images folder
    // 3) Sets the image data format (PNG or Jpg)
    // 4) Uploads the image to Firebase.
    // 5) Handle error if any. Otherwise, switch to the View Controller which shows the uses
    
    Added the prepare(for segue function.

--- Snapchat/PictureViewController.swift
+++ Snapchat/PictureViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 
+import Firebase
+
 class PictureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     // imagePicker
     
     var imagePicker = UIImagePickerController ()
-
+    
     
     // @IBOutlet
     
@@ -44,6 +46,34 @@ class PictureViewController: UIViewController, UIImagePickerControllerDelegate, 
     
     @IBAction func nextTapped(_ sender: Any) {
         
+        // The following code lines are to required to
+        // 1) Disables the Next button until the selected image is upload to the Storage of Firebase
+        // 2) Sets the images folder
+        // 3) Sets the image data format (PNG or Jpg)
+        // 4) Uploads the image to Firebase.
+        // 5) Handle error if any. Otherwise, switch to the View Controller which shows the uses 
+        
+        nextButton.isEnabled = false
+        
+        let imagesFolder = FIRStorage.storage().reference().child("images")
+        
+        let imageData = UIImagePNGRepresentation(imageView.image!)!
+        
+        imagesFolder.child("images.png").put(imageData, metadata: nil, completion: {(metadata, error) in
+            
+            if error != nil {
+                
+                print("We tried to upload!r \(error)")
+                
+            } else {
+                
+                self.performSegue(withIdentifier: "selectUserSegue", sender: nil)
+                
+            }
+        }
+            
+        )
+        
     }
     
     
@@ -51,17 +81,17 @@ class PictureViewController: UIViewController, UIImagePickerControllerDelegate, 
         
         super.viewDidLoad()
         
-        // Setting the delegate of the imagePicker to self required to work adding the following deleges to VC Class 
+        // Setting the delegate of the imagePicker to self required to work adding the following deleges to VC Class
         // 1) UIImagePickerControllerDelegate 2) UINavigationControllerDelegate)
         
         imagePicker.delegate = self
-
+        
     }
     
     // This function is required to load the image captured by camera into the imageView control.
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-
+        
         // The following code lines are to required to
         // 1) Sets the value of the image constant to the value of info
         // 2) Set the background of the imageView control to clear
@@ -75,6 +105,12 @@ class PictureViewController: UIViewController, UIImagePickerControllerDelegate, 
         imageView.backgroundColor = UIColor.clear
         
         imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        
         
     }
 }
